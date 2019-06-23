06d887936b3c201a7e54cbe21ea7e0fefdf63e76
laurent <laurent.mazare@gmail.com>
2019-06-23 18:31:18 +0100

    Improve the bot control example.

--- examples/mdev.ml
+++ examples/mdev.ml
@@ -1,32 +1,101 @@
-module B = Smbus.Bus
-
 (* Example code for the Freenove 3-wheeled smartcar.
  https://github.com/Freenove/Freenove_Three-wheeled_Smart_Car_Kit_for_Raspberry_Pi
 *)
 
-type command =
-  | IO1
-  | IO2
-  | IO3
+module Command = struct
+  type t =
+    | Servo1
+    | Servo2
+    | Servo3
+    | Servo4
+    | Pwm1
+    | Pwm2
+    | Dir1
+    | Dir2
+    | Buzzer
+    | Io1
+    | Io2
+    | Io3
+    | Sonic
 
-let command_to_int = function
-  | IO1 -> 9
-  | IO2 -> 10
-  | IO3 -> 11
+  let to_int = function
+    | Servo1 -> 0
+    | Servo2 -> 1
+    | Servo3 -> 2
+    | Servo4 -> 3
+    | Pwm1 -> 4
+    | Pwm2 -> 5
+    | Dir1 -> 6
+    | Dir2 -> 7
+    | Buzzer -> 8
+    | Io1 -> 9
+    | Io2 -> 10
+    | Io3 -> 11
+    | Sonic -> 12
+end
 
-let set_rgb bus ~r ~g ~b=
-  B.write_word_data bus (command_to_int IO1) r;
-  B.write_word_data bus (command_to_int IO2) g;
-  B.write_word_data bus (command_to_int IO3) b
+module Bot = struct
+  module B = Smbus.Bus
+
+  type t =
+    { bus : B.t
+    }
+
+  let create () =
+    let bus = B.create 1 in
+    B.set_address bus 0x18 ~force:false;
+    { bus }
+
+  let write_command t cmd v =
+    B.write_word_data t.bus (Command.to_int cmd) v
+
+  let set_rgb t ~r ~g ~b =
+    write_command t Io1 r;
+    write_command t Io2 b;
+    write_command t Io3 g
+
+  let set_buzzer t ~level = write_command t Buzzer level
+
+  let set_pwm t ~level =
+    let level =
+    if level < 0
+    then begin
+      write_command t Dir1 0;
+      write_command t Dir2 0;
+      - level
+    end else begin
+      write_command t Dir1 1;
+      write_command t Dir2 1;
+      level
+    end
+    in
+    write_command t Pwm1 level;
+    write_command t Pwm2 level
+end
 
 let () =
-  let b = B.create 1 in
-  B.set_address b 0x18 ~force:false;
-  set_rgb b ~r:0 ~g:1 ~b:1;
-  Unix.sleep 3;
-  set_rgb b ~r:1 ~g:0 ~b:1;
-  Unix.sleep 3;
-  set_rgb b ~r:1 ~g:1 ~b:0;
-  Unix.sleep 3;
-  set_rgb b ~r:1 ~g:1 ~b:1;
-  B.close b
+  let bot = Bot.create () in
+  let cmd =
+    match Sys.argv with
+    | [| _ |] | [| _; "blink" |] -> `blink
+    | [| _; "buzzer" |] -> `buzzer
+    | [| _; "forward" |] -> `forward
+    | _ -> failwith "usage: mdev.exe blink|buzzer|forward|..."
+  in
+  match cmd with
+  | `blink ->
+      Bot.set_rgb bot ~r:0 ~g:1 ~b:1;
+      Unix.sleep 3;
+      Bot.set_rgb bot ~r:1 ~g:0 ~b:1;
+      Unix.sleep 3;
+      Bot.set_rgb bot ~r:1 ~g:1 ~b:0;
+      Unix.sleep 3;
+      Bot.set_rgb bot ~r:1 ~g:1 ~b:1
+  | `buzzer ->
+      Bot.set_buzzer bot ~level:3000;
+      Unix.sleep 1;
+      Bot.set_buzzer bot ~level:0
+  | `forward ->
+      Bot.set_pwm bot ~level:1000;
+      Unix.sleep 1;
+      Bot.set_pwm bot ~level:0
