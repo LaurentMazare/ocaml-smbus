58162d025d54c4cc2a88eea25481ebeaef606242
laurent <laurent.mazare@gmail.com>
2019-07-11 21:08:31 +0100

    Robustify the distance estimation.

--- examples/mdev_corridor.ml
+++ examples/mdev_corridor.ml
@@ -27,7 +27,7 @@ module Sonic_scan : sig
 end = struct
   type t =
     { mdev : Mdev.t
-    ; distances : float array
+    ; distances : float Deque.t array
     ; angles : float list
     }
 
@@ -35,21 +35,29 @@ end = struct
     Deferred.List.iteri t.angles ~f:(fun i angle ->
         Mdev.set_servo2 t.mdev angle;
         let%map () = after (Time.Span.of_sec 0.05) in
-        t.distances.(i) <- Mdev.get_sonic t.mdev)
+        let distance = Mdev.get_sonic t.mdev in
+        Deque.enqueue_front t.distances.(i) distance;
+        if Deque.length t.distances.(i) > 3 then Deque.drop_front t.distances.(i))
 
   let create_and_start mdev ~angles ~refresh_rate ~stop =
     if not (List.is_sorted angles ~compare:Float.compare)
     then raise (Invalid_argument "angles has to be sorted");
     if List.exists angles ~f:(fun angle -> Float.( < ) angle 0. || Float.( > ) angle 1.)
     then raise (Invalid_argument "angles have to be between 0 and 1");
-    let t =
-      { mdev; distances = Array.create ~len:(List.length angles) Float.nan; angles }
-    in
+    let distances = Array.init (List.length angles) ~f:(fun _ -> Deque.create ()) in
+    let t = { mdev; distances; angles } in
     let%map () = refresh t in
     Clock_ns.every' refresh_rate ~stop:(Ivar.read stop) (fun () -> refresh t);
     t
 
-  let distances t = Array.to_list t.distances
+  let distances t =
+    Array.map t.distances ~f:(fun distances ->
+        match Deque.to_list distances |> List.sort ~compare:Float.compare with
+        | [] -> Float.nan
+        | [ l ] -> l
+        | [ l1; l2 ] -> (l1 +. l2) /. 2.
+        | l -> List.nth_exn l (List.length l / 2))
+    |> Array.to_list
 end
 
 let run () =
